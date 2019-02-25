1c4e9cc845993b8489327101972b3f54b8882be9
myj <2908037646@qq.com>
2019-02-25 17:42:53 +0800

    first commit

---
+++ src/Admin.js
@@ -0,0 +1,27 @@
+/**
+ * +----------------------------------------------------------------------
+ * | Admin | Author: 1009239228@qq.com
+ * +----------------------------------------------------------------------
+ */
+
+import React, {Component} from 'react';
+
+class Admin extends Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+
+    }
+
+}
+
+export default Admin;
