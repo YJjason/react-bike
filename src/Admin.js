d2b8395b1f899449a98aa865f2c862dba8ca9d2a
myj <2908037646@qq.com>
2019-02-28 09:32:36 +0800

    2019-2-27 共享单车后台管理系统，头部完善

--- src/Admin.js
+++ src/Admin.js
@@ -10,6 +10,7 @@ import {Row, Col} from 'antd';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import NavLeft from './components/NavLeft';
+import Home from './pages/home';
 
 import './style/common.less';
 
@@ -24,7 +25,8 @@ class Admin extends Component {
                 <Col span={21} className='main'>
                     <Header/>
                     <Row className='content'>
-                        content
+                        <Home></Home>
+                        {/* 路由 this.props.children*/}
                     </Row>
                     <Footer/>
                 </Col>
---
+++ src/pages/home/index.js
@@ -0,0 +1,22 @@
+/**
+ * +----------------------------------------------------------------------
+ * | index | Author: 1009239228@qq.com
+ * +----------------------------------------------------------------------
+ */
+
+import React,{Component} from 'react';
+
+import './index.less'
+
+class Home extends Component{
+
+    render() {
+        return (
+            <div className='home-wrap'>
+                后台管理系统
+            </div>
+        );
+    }
+
+}
+export default Home;
