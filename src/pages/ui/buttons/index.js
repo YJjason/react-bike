95220cb097146e23e658d7f78ed341b37dcef0cb
myj <2908037646@qq.com>
2019-02-28 17:26:09 +0800

    2019-2-27 共享单车后台管理系统，ui组件buttons

--- src/pages/ui/buttons/index.js
+++ src/pages/ui/buttons/index.js
@@ -3,17 +3,84 @@
  * | index | Author: 1009239228@qq.com
  * +----------------------------------------------------------------------
  */
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import {Card, Button, Icon, Radio} from "antd";
 
-class Buttons extends Component{
+import './../ui.less'
+
+class Buttons extends Component {
+
+    state = {
+        loading: false,
+        iconLoading: false,
+        size: 'default'
+    }
+
+    enterLoading = () => {
+        this.setState({loading: true});
+    }
+    enterIconLoading = () => {
+        this.setState({loading: false});
+    }
+    handleChange = (e) => {
+        this.setState({size: e.target.value})
+    }
 
     render() {
         return (
             <div>
-                buttons
+                <Card title='基础按钮'>
+                    <Button type="primary">Primary</Button>
+                    <Button>Default</Button>
+                    <Button type="dashed">Dashed</Button>
+                    <Button type="danger">Danger</Button>
+                    <Button disabled>disabled</Button>
+                </Card>
+                <Card title='图形按钮'>
+                    <Button icon="plus">创建</Button>
+                    <Button icon="edit">编辑</Button>
+                    <Button icon="delete">删除</Button>
+                    <Button icon="search" shape='circle'/>
+                    <Button icon="search">搜索</Button>
+                    <Button icon="download">下载</Button>
+                </Card>
+                <Card title='loading按钮'>
+                    <Button type="primary" loading>
+                        确认
+                    </Button>
+                    <Button type="primary" shape='circle' loading/>
+                    <Button type="primary" loading={this.state.loading} onClick={this.enterLoading}>
+                        点击加载
+                    </Button>
+                    <Button shape='circle' loading></Button>
+                    <Button type="primary" icon="poweroff" onClick={this.enterIconLoading}>
+                        Click me!
+                    </Button>
+                </Card>
+                <Card title='按钮组'>
+                    <Button.Group>
+                        <Button type="primary">
+                            <Icon type="left"/>Go back
+                        </Button>
+                        <Button type="primary">
+                            Go forward<Icon type="right"/>
+                        </Button>
+                    </Button.Group>
+                </Card>
+                <Card title='按钮尺寸'>
+                    <Radio.Group value={this.state.size} onChange={this.handleChange}>
+                        <Radio value='small'>L</Radio>
+                        <Radio value='default'>M</Radio>
+                        <Radio value='large'>R</Radio>
+                    </Radio.Group>
+                    <Button size={this.state.size}>按钮尺寸</Button>
+                    <Button size={this.state.size}>按钮尺寸</Button>
+                    <Button size={this.state.size}>按钮尺寸</Button>
+                </Card>
             </div>
         );
     }
 
 }
+
 export default Buttons;
