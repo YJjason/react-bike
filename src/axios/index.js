21394beded3a4729665f0438ad538af56501bf5b
YJjason <1009239228@qq.com>
2019-03-05 22:48:09 +0800

    管理后台 表格 easemock 数据，自动获取数据列表

--- src/axios/index.js
+++ src/axios/index.js
@@ -5,6 +5,9 @@
  */
 import JsonP from 'jsonp';
 
+import axios from 'axios';
+import {Modal} from "antd";
+
 export default class Axios {
     static jsonp(options) {
         return new Promise((resolve, reject) => {
@@ -19,4 +22,39 @@ export default class Axios {
             })
         })
     }
+
+    static ajax(options) {
+        let loading;
+        if (options.data && options.data.isShowLoading !== false) {
+            loading = document.getElementById('ajaxLoading');
+            loading.style.display = 'block';
+        }
+        let baseApi = 'https://www.easy-mock.com/mock/5c7d3aaa2d0e9237c71cca2e/mockapi';
+        return new Promise((resolve, reject) => {
+            axios({
+                url: options.url,
+                method: "GET",
+                baseURL: baseApi,
+                timeout: 5000,
+                params: (options.data && options.data.params) || ''
+
+            }).then((response) => {
+                if (response.status == '200') {
+                    let res = response.data;
+                    if (res.code == '0') {
+                        loading.style.display = 'none'
+                        resolve(res)
+                    } else {
+                        Modal.info({
+                            title: '错误提示',
+                            message: res.msg
+                        })
+                    }
+                } else {
+                    reject(response.data)
+                }
+            })
+        })
+    }
+
 }
--- src/pages/table/basicTable/index.js
+++ src/pages/table/basicTable/index.js
@@ -8,11 +8,16 @@
 
 import React, {Component} from 'react';
 import {Card, Table} from 'antd';
-import axios from 'axios';
+// import axios from 'axios';
+//使用封装的请求
+import axios from './../../../axios'
 
 class BasicTable extends Component {
 
-    state = {dataSource2: []}
+    state = {
+        dataSource2: [],
+        selectedRowKeys:[]
+    }
 
     componentDidMount() {
         this.request();
@@ -48,23 +53,58 @@ class BasicTable extends Component {
                 time: '22:14'
             },
         ]
+        dataSource.map((item, index) => {
+            item.key = index
+        })
 
         this.setState({
             dataSource
         })
+        //初始化调用
+        this.request();
     }
 
     /*动态获取mock 数据*/
 
     request = () => {
-        let baseUrl = 'https://www.easy-mock.com/mock/5c7d3aaa2d0e9237c71cca2e/mockapi'
-        axios.get(baseUrl + '/table/list').then((res) => {
-            console.log(res);
+        /* let baseUrl = 'https://www.easy-mock.com/mock/5c7d3aaa2d0e9237c71cca2e/mockapi'*/
+        /*封裝請求*/
+        /*    axios.get(baseUrl + '/table/list').then((res) => {
+                if (res.status === 200 && res.data.code === 0) {
+                    this.setState({
+                        dataSource2: res.data.result
+                    })
+                }
+            })*/
+
+        axios.ajax({
+            url: '/table/list',
+            data: {
+                params: {
+                    page: 1
+                }
+            }
+        }).then((res) => {
+            if (res.code == 0) {
+                res.result.map((item, index) => {
+                    item.key = index
+                })
+                this.setState({
+                    dataSource2: res.result
+                })
+            }
+        })
+    }
+
+    onRowClick = (record, index) => {
+        let selectKey = [index];
+        this.setState({
+            selectedRowKey: selectKey,
+            selectedItem:record
         })
     }
 
     render() {
-
         const columns = [
             {
                 title: 'id',
@@ -79,14 +119,37 @@ class BasicTable extends Component {
                 title: '性别',
                 dataIndex: 'sex',
                 key: 'age',
+                render(sex) {
+                    return sex = 1 ? '男' : '女'
+                }
             }, {
                 title: '状态',
                 dataIndex: 'state',
-                key: 'state'
+                key: 'state',
+                render(state) {
+                    let config = {
+                        '1': '城南',
+                        '2': '盘门',
+                        '3': '相门',
+                        '4': '姑苏',
+                        '5': '大闸蟹'
+                    }
+                    return config[state]
+                }
             }, {
                 title: '爱好',
                 dataIndex: 'intrest',
                 key: 'intrest',
+                render(intrest) {
+                    let config = {
+                        '1': '游泳',
+                        '2': '篮球',
+                        '3': '爬山',
+                        '4': '音乐',
+                        '5': '电影'
+                    }
+                    return config[intrest]
+                }
             }, {
                 title: '生日',
                 dataIndex: 'birthday',
@@ -100,7 +163,17 @@ class BasicTable extends Component {
                 dataIndex: 'time',
                 key: 'time'
             }
-        ]
+        ];
+      /*  const rowSelection = {
+            type: 'radio',
+            selectedRowKey
+        };
+*/
+        const { selectedRowKeys } = this.state;
+        const rowSelection = {
+            type:'radio',
+            selectedRowKeys,
+        };
 
         return (
             <div>
@@ -109,8 +182,20 @@ class BasicTable extends Component {
                            dataSource={this.state.dataSource}
                     />
                 </Card>
-                <Card title='动态数据渲染表格'>
+                <Card title='动态mock数据渲染表格'>
                     <Table columns={columns}
+                           dataSource={this.state.dataSource2}
+                    />
+                </Card>
+                <Card title='mock 单选表格'>
+                    <Table columns={columns}
+                           bordered
+                           rowSelection={rowSelection}
+                           onRow={(record, index) => {
+                               return {
+                                   onClick: this.onRowClick(record, index),       // 点击行
+                               };
+                           }}
                            dataSource={this.state.dataSource2}
                     />
                 </Card>
